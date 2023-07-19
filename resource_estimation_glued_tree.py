f3146cae681d52c7a5db5788e54c1a8d5499b635
jli0108 <jli0108@umd.edu>
2023-07-19 17:31:52 -0400

    add resource estimate scripts

---
+++ resource_estimation_glued_tree.py
@@ -0,0 +1,186 @@
+import numpy as np
+from utils import *
+from resource_estimate_utils import *
+from os.path import join
+from time import time
+import multiprocessing
+import networkx as nx
+from joblib import Parallel, delayed
+from random import shuffle
+
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.synthesis import LieTrotter, SuzukiTrotter
+from qiskit import transpile
+from qiskit.circuit.library import PauliEvolutionGate
+
+from braket.devices import LocalSimulator
+
+def get_glued_tree(h):
+
+    # Two binary trees of height h (2^(h+1) - 1 nodes each glued together)
+    num_nodes_per_binary_tree = 2 ** (h+1) - 1
+    num_nodes = 2 * num_nodes_per_binary_tree
+    graph = nx.Graph()
+
+    # Leaves
+    leaves_first = []
+    leaves_second = []
+    for i in range(2 ** h):
+        leaves_first.append(2 ** h - 1 + i)
+        leaves_second.append(num_nodes - 1 - (2 ** h - 1) - i)
+
+    for i in np.arange(1, num_nodes_per_binary_tree):
+
+        # First binary tree
+        graph.add_edge(int((i-1)/2), i)
+
+        # Second binary tree
+        graph.add_edge(num_nodes - 1 - int((i-1)/2), num_nodes - 1 - i)
+
+    # Glue the two trees together
+    # Shuffle the leaves to get a random cycle
+    shuffle(leaves_first)
+    shuffle(leaves_second)
+
+    for i in range(2 ** h):
+        graph.add_edge(leaves_first[i], leaves_second[i])
+        graph.add_edge(leaves_second[i], leaves_first[(i+1) % (2 ** h)])
+
+    return graph
+
+def get_binary_resource_estimate(h, dimension, error_tol, order=1):
+    
+    
+    graph = get_glued_tree(h)
+    N = graph.order()
+    print(f"h = {h}, N = {N}", flush=True)
+
+    adjacency_matrix = nx.adjacency_matrix(graph, nodelist=sorted(graph.nodes())).toarray()
+    adjacency_matrix_padded = np.pad(adjacency_matrix, (0, 2 ** int(np.ceil(np.log2(N))) - N))
+
+    pauli_op = SparsePauliOp.from_operator(adjacency_matrix_padded)
+
+    if order == 1:
+        circuit = LieTrotter(reps=1).synthesize(PauliEvolutionGate(pauli_op.group_commuting()))
+    elif order == 2:
+        circuit = SuzukiTrotter(order=2, reps=1).synthesize(PauliEvolutionGate(pauli_op.group_commuting()))
+    else:
+        raise ValueError("Order must be 1 or 2")
+
+    compiled_circuit = transpile(circuit, basis_gates=['rxx', 'rx', 'ry'], optimization_level=3)
+
+    ops = compiled_circuit.count_ops()
+
+    num_single_qubit_gates, num_two_qubit_gates = get_gate_counts(ops)
+
+    assert num_single_qubit_gates + num_two_qubit_gates == sum(ops.values())
+    assert num_two_qubit_gates == compiled_circuit.num_nonlocal_gates()
+
+    print(f"N = {N}, num two qubit gates:", num_two_qubit_gates)
+
+    # # Estimate number of Trotter steps required
+    r_min, r_max = 1, 10
+    while std_bin_trotter_error(adjacency_matrix_padded, pauli_op, r_max) > error_tol:
+        r_max *= 2
+
+    # binary search for r
+    while r_max - r_min > 1:
+        r = (r_min + r_max) // 2
+        if std_bin_trotter_error(adjacency_matrix_padded, pauli_op, r) > error_tol:
+            r_min = r
+        else:
+            r_max = r
+    
+    print(f"Finished N={N}, num two qubit gates={num_two_qubit_gates}, trotter steps={r_max}", flush=True)
+    return num_two_qubit_gates, r_max
+
+if __name__ == "__main__":
+    
+    print("Resource estimation for QW on glued tree.")
+
+    num_jobs = 64
+    print("Number of jobs:", num_jobs)
+
+    dimension = 1
+    error_tol = 1e-2
+    trotter_order = 2
+    T = 1
+    print(f"Error tolerance: {error_tol : 0.2f}.")
+    print(f"Trotter order: {trotter_order}")
+
+    h_vals_binary = np.arange(1,6)
+    N_vals_binary = 2 * (2 ** (h_vals_binary + 1) - 1)
+    binary_trotter_steps = np.zeros(len(N_vals_binary))
+    binary_two_qubit_gate_count_per_trotter_step = np.zeros(len(N_vals_binary))
+
+    h_vals_one_hot = np.arange(1, 3)
+    N_vals_one_hot = 2 * (2 ** (h_vals_one_hot + 1) - 1)
+    one_hot_trotter_steps = np.zeros(len(N_vals_one_hot), dtype=int)
+    one_hot_two_qubit_gate_count_per_trotter_step = np.zeros(len(N_vals_one_hot))
+
+
+    print("Running resource estimation for standard binary encoding")
+    for i, h in enumerate(h_vals_binary):
+
+        res = Parallel(n_jobs=num_jobs)(delayed(get_binary_resource_estimate)(h, dimension, error_tol, order=trotter_order) for h in h_vals_binary)
+        binary_two_qubit_gate_count_per_trotter_step, binary_trotter_steps = zip(*res)
+        binary_two_qubit_gate_count_per_trotter_step = np.array(binary_two_qubit_gate_count_per_trotter_step)
+        binary_trotter_steps = np.array(binary_trotter_steps, dtype=int)
+
+        np.savez(join("resource_data", "resource_estimation_glued_tree_binary.npz"),
+                N_vals_binary=N_vals_binary,
+                binary_trotter_steps=binary_trotter_steps,
+                binary_two_qubit_gate_count_per_trotter_step=binary_two_qubit_gate_count_per_trotter_step)
+
+
+    # One hot encoding
+    print("Running resource estimation for one-hot encoding", flush=True)
+    encoding = "one-hot"
+    device = LocalSimulator()
+    num_samples = 100
+
+    for i, h in enumerate(h_vals_one_hot):
+        start_time = time()
+
+        graph = get_glued_tree(h)
+        N = graph.order()
+        n = num_qubits_per_dim(N, encoding)
+        codewords = get_codewords_1d(n, encoding, periodic=False)
+
+        print(f"Running h = {h}, N = {N} for one-hot", flush=True)
+        adjacency_matrix = nx.adjacency_matrix(graph, nodelist=sorted(graph.nodes())).toarray()
+
+        tol = 0.5
+        lamb = n
+        codewords = get_codewords_1d(n, encoding, periodic=False)
+
+        one_hot_two_qubit_gate_count_per_trotter_step[i] = graph.size()
+
+        # Check the fidelity
+        A, B = get_H_one_hot(n, lamb, adjacency_matrix)
+    
+        # Estimate number of Trotter steps required
+        r_min, r_max = 1, 10
+        if i > 0:
+            r_min = one_hot_trotter_steps[i-1]
+            r_max = one_hot_trotter_steps[i-1] * 2
+        while estimate_trotter_error(N, A+B, one_hot_get_trotter_step_circ(N, dimension, T, r_max, adjacency_matrix, lamb, order=trotter_order), r_max, dimension, encoding, codewords, device, num_samples, num_jobs) > error_tol:
+            r_max *= 2
+
+        # binary search for r
+        while r_max - r_min > 1:
+            r = (r_min + r_max) // 2
+            if estimate_trotter_error(N, A+B, one_hot_get_trotter_step_circ(N, dimension, T, r, adjacency_matrix, lamb, order=trotter_order), r, dimension, encoding, codewords, device, num_samples, num_jobs) > error_tol:
+                r_min = r
+            else:
+                r_max = r
+
+        one_hot_trotter_steps[i] = r_max
+
+        # Save data
+        np.savez(join("resource_data", "resource_estimation_glued_tree_one_hot.npz"),
+                 N_vals_one_hot=N_vals_one_hot[:i+1],
+                 one_hot_trotter_steps=one_hot_trotter_steps[:i+1],
+                 one_hot_two_qubit_gate_count_per_trotter_step=one_hot_two_qubit_gate_count_per_trotter_step[:i+1])
+
+        print(f"Finished N = {N}, time = {time() - start_time} seconds.", flush=True)
---
+++ resource_estimation_spatial_search.py
@@ -0,0 +1,272 @@
+import numpy as np
+
+from utils import *
+from resource_estimate_utils import *
+from os.path import join
+from time import time
+import multiprocessing
+
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.synthesis import LieTrotter, SuzukiTrotter
+from qiskit import transpile
+from qiskit.circuit.library import PauliEvolutionGate
+
+from braket.devices import LocalSimulator
+
+def get_binary_resource_estimate(N, dimension, error_tol, order=1):
+    print(f"N = {N}", flush=True)
+
+    # Compute the optimal gamma
+    L = get_laplacian_lattice(N, d=dimension)
+    marked_vertex_1 = np.zeros(N)
+    marked_vertex_1[N-1] = 1
+    marked_vertex_2 = np.zeros(N)
+    marked_vertex_2[0] = 1
+
+    marked_vertex = np.kron(marked_vertex_1, marked_vertex_2)
+    H_oracle = - csc_matrix(np.outer(marked_vertex, marked_vertex))
+    
+    # Sign is flipped here; this function minimizes the difference between the two largest eigenvalues of gamma * L + H_oracle
+    gamma = scipy_get_optimal_gamma(L, -H_oracle, 0.3)
+
+    H_spatial_search = (-gamma * L + H_oracle).toarray()
+    H_spatial_search_padded = np.pad(H_spatial_search, (0, 2 ** int(np.ceil(np.log2(N ** 2))) - N ** 2))
+    pauli_op = SparsePauliOp.from_operator(H_spatial_search_padded)
+    
+    if order == 1:
+        circuit = LieTrotter(reps=1).synthesize(PauliEvolutionGate(pauli_op.group_commuting()))
+    elif order == 2:
+        circuit = SuzukiTrotter(order=2, reps=1).synthesize(PauliEvolutionGate(pauli_op.group_commuting()))
+    else:
+        raise ValueError("Order must be 1 or 2")
+
+    compiled_circuit = transpile(circuit, basis_gates=['rxx', 'rx', 'ry'], optimization_level=3)
+
+    ops = compiled_circuit.count_ops()
+
+    num_single_qubit_gates, num_two_qubit_gates = get_gate_counts(ops)
+
+    assert num_single_qubit_gates + num_two_qubit_gates == sum(ops.values())
+    assert num_two_qubit_gates == compiled_circuit.num_nonlocal_gates()
+
+
+    # Estimate number of Trotter steps required
+    r_min, r_max = 1, 10
+    while std_bin_trotter_error(H_spatial_search_padded, pauli_op, r_max) > error_tol:
+        r_max *= 2
+
+    # binary search for r
+    while r_max - r_min > 1:
+        r = (r_min + r_max) // 2
+        if std_bin_trotter_error(H_spatial_search_padded, pauli_op, r) > error_tol:
+            r_min = r
+        else:
+            r_max = r
+    
+
+    return num_two_qubit_gates, r_max
+
+def get_H_spatial_search(n, lamb, gamma, encoding):
+    if encoding == "antiferromagnetic":
+        assert n % 2 == 1, "only works for odd number of qubits"
+    J = np.zeros((2 * n, 2 * n))
+    h = np.zeros(2 * n)
+
+    for i in range(2):
+        h[i * n] = 1
+        if encoding == "unary":
+            h[(i + 1) * n - 1] = -1
+        else:
+            h[(i + 1) * n - 1] = (-1) ** (n)
+        for j in np.arange(i * n, (i + 1) * n - 1):
+            if encoding == "unary":
+                J[j, j + 1] = -1
+            else:
+                J[j, j + 1] = 1
+
+    H_pen = lamb * (sum_J_zz(2 * n, J) + sum_h_z(2 * n, h))
+
+    # Create the oracle Hamiltonian
+    V = np.zeros((2 * n, 2 * n))
+    V[n-1, n] = 1
+
+    h = np.zeros(2 * n)
+    h[n-1] = -1
+    
+    H_oracle = sum_delta_n(2 * n, h) + sum_V_nn(2 * n, V)
+
+    # Correction term for Laplacian
+    h_correction = np.zeros(2 * n)
+    for i in range(2):
+        h_correction[i * n] = -1
+        if encoding == "unary":
+            h_correction[(i + 1) * n - 1] = 1
+        else:
+            h_correction[(i + 1) * n - 1] = (-1) ** (n+1)
+    
+    return H_pen + H_oracle - gamma * sum_delta_n(2 * n, h_correction), - gamma * sum_x(2 * n)
+
+def get_spatial_search_one_trotter_step_circuit(n, lamb, gamma, T, r, encoding, order=1):
+    one_step_circ = Circuit()
+    
+    if order == 1:
+        # x rotations
+        for i in range(2 * n):
+            one_step_circ.rx(i, - 2 * gamma * T / r)
+
+        # penalty term
+        for i in range(2):
+            one_step_circ.rz(i * n, 2 * lamb * T / r)
+            if encoding == "unary":
+                one_step_circ.rz((i + 1) * n - 1, - 2 * lamb * T / r)
+            if encoding == "antiferromagnetic":
+                one_step_circ.rz((i + 1) * n - 1, (-1) ** (n) * 2 * lamb * T / r)
+            
+            for j in np.arange(i * n, (i + 1) * n - 1):
+                if encoding == "unary":
+                    one_step_circ.zz(j, j+1, -2 * lamb * T / r)
+                elif encoding == "antiferromagnetic":
+                    one_step_circ.zz(j, j+1, 2 * lamb * T / r)
+
+        # oracle term
+        one_step_circ.xx(n-1, n, (T / r) / 2)
+        one_step_circ.rx(n-1, (T / r) / 2)
+        one_step_circ.rx(n, -(T / r) / 2)
+
+        # laplacian correction term
+        for i in range(2):
+            one_step_circ.rz(i * n, -gamma * T / r)
+            if encoding == "unary":
+                one_step_circ.rz((i + 1) * n - 1, gamma * T / r)
+            elif encoding == "antiferromagnetic":
+                one_step_circ.rz((i + 1) * n - 1, (-1) ** (n+1) * gamma * T / r)
+
+    
+    elif order == 2:
+        # x rotations
+        for i in range(2 * n):
+            one_step_circ.rx(i, - gamma * T / r)
+
+        # penalty term
+        for i in range(2):
+            one_step_circ.rz(i * n, 2 * lamb * T / r)
+            if encoding == "unary":
+                one_step_circ.rz((i + 1) * n - 1, - 2 * lamb * T / r)
+            if encoding == "antiferromagnetic":
+                one_step_circ.rz((i + 1) * n - 1, (-1) ** (n) * 2 * lamb * T / r)
+            
+            for j in np.arange(i * n, (i + 1) * n - 1):
+                if encoding == "unary":
+                    one_step_circ.zz(j, j+1, -2 * lamb * T / r)
+                elif encoding == "antiferromagnetic":
+                    one_step_circ.zz(j, j+1, 2 * lamb * T / r)
+
+        # oracle term
+        one_step_circ.zz(n-1, n, (T / r) / 2)
+        one_step_circ.rz(n-1, (T / r) / 2)
+        one_step_circ.rz(n, -(T / r) / 2)
+
+        # laplacian correction term
+        for i in range(2):
+            one_step_circ.rz(i * n, -gamma * T / r)
+            if encoding == "unary":
+                one_step_circ.rz((i + 1) * n - 1, gamma * T / r)
+            elif encoding == "antiferromagnetic":
+                one_step_circ.rz((i + 1) * n - 1, (-1) ** (n+1) * gamma * T / r)
+            
+        # x rotations
+        for i in range(2 * n):
+            one_step_circ.rx(i, - gamma * T / r)
+    else:
+        raise ValueError("Trotter order must be 1 or 2")
+
+    return one_step_circ
+
+if __name__ == "__main__":
+    
+    print("Resource estimation for spatial search.")
+    num_jobs = 64
+    print("Number of jobs:", num_jobs)
+
+    dimension = 2
+    error_tol = 1e-2
+    trotter_order = 2
+    T = 1
+    print(f"Error tolerance: {error_tol : 0.2f}.")
+    print(f"Trotter order: {trotter_order}")
+
+    N_vals_binary = np.arange(4, 32)
+    N_vals_unary = np.arange(4, 9)
+
+    unary_trotter_steps = np.zeros(len(N_vals_unary), dtype=int)
+    unary_two_qubit_gate_count_per_trotter_step = np.zeros(len(N_vals_unary))
+
+    print("Running resource estimation for standard binary encoding", flush=True)
+    res = Parallel(n_jobs=num_jobs)(delayed(get_binary_resource_estimate)(N, dimension, error_tol) for N in N_vals_binary)
+    binary_two_qubit_gate_count_per_trotter_step, binary_trotter_steps = zip(*res)
+    binary_two_qubit_gate_count_per_trotter_step = np.array(binary_two_qubit_gate_count_per_trotter_step)
+    binary_trotter_steps = np.array(binary_trotter_steps, dtype=int)
+
+    print(binary_two_qubit_gate_count_per_trotter_step)
+    print(binary_trotter_steps)
+    np.savez(join("resource_data", "resource_estimation_spatial_search_binary.npz"),
+                N_vals_binary=N_vals_binary,
+                binary_trotter_steps=binary_trotter_steps,
+                binary_two_qubit_gate_count_per_trotter_step=binary_two_qubit_gate_count_per_trotter_step)
+
+
+    # Antiferromagnetic encoding
+    encoding = "unary"
+    print(f"Running resource estimation for {encoding} encoding", flush=True)
+    device = LocalSimulator()
+    num_samples = 100
+
+    for i, N in enumerate(N_vals_unary):
+
+        print(f"Running N = {N}", flush=True)
+        start_time = time()
+        n = num_qubits_per_dim(N, encoding)
+
+        # Compute the optimal gamma
+        L = get_laplacian_lattice(N, d=dimension)
+        marked_vertex_1 = np.zeros(N)
+        marked_vertex_1[N-1] = 1
+        marked_vertex_2 = np.zeros(N)
+        marked_vertex_2[0] = 1
+
+        marked_vertex = np.kron(marked_vertex_1, marked_vertex_2)
+        H_oracle = - csc_matrix(np.outer(marked_vertex, marked_vertex))
+        # Sign is flipped here; this function minimizes the difference between the two largest eigenvalues of gamma * L + H_oracle
+        gamma = scipy_get_optimal_gamma(L, -H_oracle, 0.3)
+
+        codewords = get_codewords_2d(n, encoding, periodic=False)
+
+        lamb = dimension * n
+        
+        unary_two_qubit_gate_count_per_trotter_step[i] = 2 * (n - 1) + 1
+
+        A, B = get_H_spatial_search(n, lamb, gamma, encoding)
+
+        # Estimate number of Trotter steps required
+        r_min, r_max = 1, 10
+        while estimate_trotter_error(N, A + B, get_spatial_search_one_trotter_step_circuit(n, lamb, gamma, T, r_max, encoding, order=trotter_order), r_max, dimension, encoding, codewords, device, num_samples, num_jobs) > error_tol:
+            r_max *= 2
+
+        # binary search for r
+        while r_max - r_min > 1:
+            r = (r_min + r_max) // 2
+            if estimate_trotter_error(N, A + B, get_spatial_search_one_trotter_step_circuit(n, lamb, gamma, T, r, encoding, order=trotter_order), r, dimension, encoding, codewords, device, num_samples, num_jobs) > error_tol:
+                r_min = r
+            else:
+                r_max = r
+        
+        unary_trotter_steps[i] = r
+
+        np.savez(join("resource_data", f"resource_estimation_spatial_search_{encoding}.npz"),
+                    N_vals_unary=N_vals_unary[:i+1],
+                    unary_trotter_steps=unary_trotter_steps[:i+1],
+                    unary_two_qubit_gate_count_per_trotter_step=unary_two_qubit_gate_count_per_trotter_step[:i+1])
+        
+        
+        print(f"Finished N = {N}, time = {time() - start_time} seconds.", flush=True)
+    
