61ec5c134e24a572f4d22b716c380b9b6d25db55
jiaqileng <>
2023-11-15 23:00:49 -0800

    update quera plots

--- real_space_sim/QuEra/RealSpace3Dbar.m
+++ real_space_sim/QuEra/RealSpace3Dbar.m
@@ -1,4 +1,4 @@
-load("data_quera.mat")
+load("data.mat")
 
 n = 6;
 N = n + 1;
@@ -14,49 +14,49 @@ set(0,'DefaultAxesFontSize', 10, ...
 for i = 1:3
     figure(i);
    
-    time = end_times(i);
-    z_data = reshape(ideal_data(i,:,:), N, N);
+    time = sim_end_times(i);
+    z_data = reshape(sim_dists(i,:,:), N, N);
     b = bar3(z_data);
-    zlim([0 250])    % Set Z-axis range
+    zlim([0 0.25])    % Set Z-axis range
     ax = gca;
     ax.FontSize = 10;
     %ax.XTickLabel = {'|00001\rangle','|00010\rangle','|00100\rangle','|01000\rangle', '|10000\rangle'};
     %ax.YTickLabel = {'|00001\rangle','|00010\rangle','|00100\rangle','|01000\rangle', '|10000\rangle'};
     ax.XTickLabelRotation = -30;
-    title_text = sprintf('T = %.1f $$\\mu s$$', time);
+    title_text = sprintf('T = %.1f', time * 1e6);
     title(title_text, 'Interpreter', 'latex', 'Fontsize', 16)
     
     colormap(summer)
-    clim([0 250])
+    clim([0 0.25])
     for k = 1:length(b)
         b(k).CData = b(k).ZData;
         b(k).FaceColor = 'interp';
     end
     colorbar();
     f = gcf;
-    file_name = sprintf("ideal_%d.png", i);
+    file_name = sprintf("sim_%d.png", i);
     exportgraphics(f,file_name,'Resolution',300)
 end
 
 
-%% IonQ subplots
+%% QuEra subplots
 
 for i = 1:3
     figure(i);
-    time = end_times(i);
-    z_data = reshape(quera_data(i,:,:), N, N);
+    time = quera_end_times(i);
+    z_data = reshape(quera_dists(i,:,:), N, N);
     b = bar3(z_data);
-    zlim([0 120])    % Set Z-axis range
+    zlim([0 0.2])    % Set Z-axis range
     ax = gca;
     ax.FontSize = 10;
     %ax.XTickLabel = {'|00001\rangle','|00010\rangle','|00100\rangle','|01000\rangle', '|10000\rangle'};
     %ax.YTickLabel = {'|00001\rangle','|00010\rangle','|00100\rangle','|01000\rangle', '|10000\rangle'};
     ax.XTickLabelRotation = -30;
-    title_text = sprintf('T = %.1f $$\\mu s$$', time);
+    title_text = sprintf('T = %.1f', time * 1e6);
     title(title_text, 'Interpreter', 'latex', 'Fontsize', 16)
     
     colormap(parula)
-    clim([0 120])
+    clim([0 0.2])
     for k = 1:length(b)
         b(k).CData = b(k).ZData;
         b(k).FaceColor = 'interp';
---
+++ real_space_sim/QuEra/npz2mat.py
@@ -0,0 +1,10 @@
+from scipy.io import savemat
+import numpy as np
+import glob
+import os
+npzFiles = glob.glob("*.npz")
+for f in npzFiles:
+    fm = os.path.splitext(f)[0]+'.mat'
+    d = np.load(f)
+    savemat(fm, d)
+    print('generated ', fm, 'from', f)
