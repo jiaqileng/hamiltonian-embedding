e22cdf83fba000296e7f828cd9f1c8cdaf4ee62a
jli0108 <jli0108@umd.edu>
2024-05-15 14:20:14 -0400

    fix std error for kinetic energy, update figures

--- src/experiments/real_space_sim/IonQ/plot_ionq_kinetic_energy.py
+++ src/experiments/real_space_sim/IonQ/plot_ionq_kinetic_energy.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join, dirname
 
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": "Helvetica",
+})
+
 # Load plot data
 with np.load(join(dirname(__file__), 'data.npz')) as data:
     expected_position_analytical = data['expected_position_analytical']
@@ -30,15 +36,16 @@ kinetic_energy_analytical_high_res = (1+a)/4 - (0.5 * a * analytical_exp_x_sq_hi
 # plt.rcParams['font.family'] = 'Helvetica'
 plt.figure()
 # plt.plot(t_vals, kinetic_energy_analytical, 's-', color="violet", )
-plt.plot(t_vals_high_res, kinetic_energy_analytical_high_res, 's-', markevery=range(num_time_points * plot_every)[::plot_every], color="violet", linewidth=1, label="Closed-form solution for " + r"$\frac{1}{2}\langle\hat{p}\rangle_t$")
+plt.plot(t_vals_high_res, kinetic_energy_analytical_high_res, 's-', markevery=range(num_time_points * plot_every)[::plot_every], color="violet", linewidth=1, label="Closed-form solution for " + r"$\frac{1}{2}\langle\hat{p}^2\rangle_t$")
 # plt.plot(t_vals, ideal_data, '-o', color="violet", label="Hamiltonian embedding")
 # plt.plot(t_vals, ideal_data, 'ro', label="Numerical simulation (5-level subsystem)", markersize=5)
 plt.errorbar(t_vals, ionq_data, ionq_err, fmt='--o', color='blue', ecolor='skyblue', label="Experiment on IonQ (one-hot embedding)", capsize=4)
 plt.ylabel(r"Expected kinetic energy $\frac{1}{2}\hat{p}^2$", fontsize=12)
-plt.xlabel(r"$t$ (evolution time)", fontsize=12)
-plt.xticks(fontsize=8)
-plt.yticks(fontsize=8)
+plt.xlabel(r"$t$ (evolution time)", fontsize=13)
+plt.xticks(fontsize=11)
+plt.yticks(fontsize=11)
 plt.ylim(0.15, 0.625)
+plt.title(r"Observable: $\frac{1}{2}\hat{p}^2$", fontsize=22, y=1.02)
 plt.legend(fontsize=11)
-plt.savefig(join(dirname(__file__), 'real_space_ionq_kinetic_energy.png'), dpi=300)
+plt.savefig(join(dirname(__file__), 'real_space_ionq_kinetic_energy.pdf'), bbox_inches='tight')
 # plt.show()
--- src/experiments/real_space_sim/IonQ/plot_potential_function.py
+++ src/experiments/real_space_sim/IonQ/plot_potential_function.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join, dirname
 
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": "Helvetica",
+})
+
 T = 5
 a = 2
 b = -1/2
@@ -13,14 +19,14 @@ normalize_c = (1 / np.pi) ** 0.5
 init_state = normalize_c * np.exp(- x**2)
 plt.plot(x, potential, 'r--', label=r'Potential field: $f(x) = x^2 - \frac{1}{2}x$', linewidth=3)
 plt.plot(x, init_state, '-', color='navy', label='Initial wave packet (vacuum state)', linewidth=3)
-plt.xlabel('x', fontsize=14)
-plt.ylabel('y', fontsize=14)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
+plt.xlabel(r'$x$', fontsize=14)
+plt.ylabel(r'$y$', fontsize=14)
+plt.xticks(fontsize=11)
+plt.yticks(fontsize=11)
 plt.legend(fontsize=12)
 plt.ylim(-0.15, 4)
 bottom, top = plt.ylim()
 plt.vlines(x=0, ymin=bottom, ymax=2.5)
 plt.text(-0.25,2.6, r"$\langle \hat{x} \rangle = 0$", fontsize=14)
-plt.savefig(join(dirname(__file__), 'real_space_potential.png'), dpi=300)
+plt.savefig(join(dirname(__file__), 'real_space_potential.pdf'), bbox_inches="tight")
 # plt.show()
